a953bcfe9de938df0e899b7fa7dda2a9438e53d0
robinsontrejos78 <robinsontrejos78@gmail.com>
2020-02-25 21:37:09 -0500

    Update formclient.blade.php

--- resources/views/inscripcion/formclient.blade.php
+++ resources/views/inscripcion/formclient.blade.php
@@ -21,13 +21,12 @@
     <div class="login-box">
        
 
-                    <li>{{ $error }}</li>
         @if (count($errors) > 0)
             <div class="alert alert-danger">
                 <strong>Whoops!</strong> Hay problema con los datos ingresados<br><br>
                 <ul>
                     @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
+                       
                     @endforeach
                 </ul>
             </div>
