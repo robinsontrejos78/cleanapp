5f8595839be422cfcae5bb70e3ccdb817c6acb2d
robinsontrejos78 <robinsontrejos78@gmail.com>
2020-03-01 22:42:18 -0500

    ok
    ok

--- resources/views/inscripcion/formprof.blade.php
+++ resources/views/inscripcion/formprof.blade.php
@@ -71,7 +71,7 @@
                 <div class="form-group">
                   <label class="col-sm-2 control-label">Fecha de Nacimiento</label>
                   <div class="col-sm-10">
-                    <input type="time" class="form-control" id="fnaciprof" placeholder="Fecha Nacimiento">
+                    <input type="date" class="form-control" id="fnaciprof" placeholder="Fecha Nacimiento">
                   </div>
                 </div>
                  <div class="form-group">
