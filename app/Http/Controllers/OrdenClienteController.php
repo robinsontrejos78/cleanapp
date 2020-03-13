d7f4d2c48946e280a2a6bee6e0a621f99aa01e6e
omontealegre <oscarmontealegre@gmail.com>
2020-03-12 21:41:41 -0500

    OK
    OK

--- app/Http/Controllers/OrdenClienteController.php
+++ app/Http/Controllers/OrdenClienteController.php
@@ -91,6 +91,7 @@ class OrdenClienteController extends Controller
         ->join('role_user', 'user_id', '=', 'users.id')
         ->whereRaw(" users.id NOT IN ( select users.id from users inner join role_user on user_id=users.id inner join ORDEN_SERVICIOS on user_id=ORD_USR_ID where role_id=3 and ORD_LOO_ESTADOORDEN = 1 and ((ORD_INICIOORDEN - INTERVAL 30 MINUTE) <= '".$horaInicial."' and '".$horaInicial."'<= (ORD_FINORDEN + INTERVAL 30 MINUTE) or (ORD_INICIOORDEN  - INTERVAL 30 MINUTE) <= '".$horaFinal."' and '".$horaFinal."' <= (ORD_FINORDEN + INTERVAL 30 MINUTE) ) )")
         ->where('role_id', 3)
+        ->where('USR_ESTADO',1)
         ->take(5)
         ->get();
 
